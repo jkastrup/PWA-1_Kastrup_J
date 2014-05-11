10272bba7e5bed50ef2ecb99eb38ea5c8a09685c
Jason <jason.kastrup@gmail.com>
2014-05-11 17:13:02 -0400

    Wk 1 Functions Practice - main.js updated
    Deleted commented instructions

--- Wk 1/goal2_assign_jspractice/goal2_assign_JSpractice/js/main.js
+++ Wk 1/goal2_assign_jspractice/goal2_assign_JSpractice/js/main.js
@@ -7,46 +7,6 @@ Date:		May 11, 2014
 // self-executing function
 (function(){
 
-    /*******************************************
-
-     STUDENTS ASSIGNMENT
-
-     1.  create a function named 'avgNumbers'
-         - accept 1 parameter into the function that will be an array of unlimited numbers
-         - find the average of all the numbers
-         - return the average from the function
-         - console.log the answer outside of the function
-
-     2.  create a function named 'fullName'
-         - accept 2 parameters into the function that are strings (firstname and lastname)
-         - return the name after it has been concatenated
-         - console.log the answer outside of the function
-
-     3.  create a function named 'wordCount'
-         - accept 1 parameter into the function that is a long string of text words
-         - create a function that counts all the words and return the answer
-         - console.log the answer outside of the function
-
-     4.  create a function named 'charCount'
-         - accept 1 parameter into the function that is a long string of text
-         - return length of the array of string characters
-         - console.log the answer outside of the function
-
-     5.  create a function named 'vowelsInWord'
-         - accept 1 parameter into the function that is a a one word string
-         - return the number of vowels in the word
-         - console.log the answer outside of the function
-
-     6.  create a function named 'findNum'
-         - accepts 2 parameters into the function - 1. array of numbers, 2. boolean
-         - if the second parameter being passed is "false" or null then
-             - create an array with all of the odd numbers from
-                the array
-         - else - create an array with all of the even numbers
-            from the array
-         - return the array
-         - console.log the answer outside of the function
-     ********************************************/
 
     console.log('------ Goal2: Assignment: JavaScript Practice ----------');
 
