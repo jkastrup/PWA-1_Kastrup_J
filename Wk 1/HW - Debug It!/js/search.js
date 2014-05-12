005444ff2b98646759fa1bf863807ed667c30eec
Jason <jason.kastrup@gmail.com>
2014-05-11 22:52:16 -0400

    HW1 Debug It! Completed
    search.js no longer contains any syntactic errors or causes run-time
    errors.

--- Wk 1/HW - Debug It!/js/search.js
+++ Wk 1/HW - Debug It!/js/search.js
@@ -43,23 +43,23 @@
 	var search = function(query){
 		
 		// split the user's search query string into an array
-		var queryArray = query.join(" ");
+		var queryArray = query.split(" ");
 		
 		// array to store matched results from database.js
 		var results = [];
 
 		// loop through each index of db array
-		for(var i=0; j=db.length; i<j; i++){
+		for(var i=0, j=db.length; i<j; i++){
 		
 			// each db[i] is a single video item, each title ends with a pipe "|"
 			// save a lowercase variable of the video title
 			var dbTitleEnd = db[i].indexOf('|');
-			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+			var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);
 			
 			// loop through the user's search query words
 			// save a lowercase variable of the search keyword
 			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-				var qitem = queryArray[ii].tolowercase();
+				var qitem = queryArray[ii].toLowerCase();
 				
 				// is the keyword anywhere in the video title?
 				// If a match is found, push full db[i] into results array
