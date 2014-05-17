11a87c6e3394f31ff35ff6c86b7cfbf57cb3cea7
Jason <jason.kastrup@gmail.com>
2014-05-16 22:05:30 -0400

    Wk 2 Guessing Game
    Game works correctly, with counter.
    Displays proper outputs, and checks for proper inputs.
    Still working on Enter key shortcut

--- Wk 2/goal4_assign_guessing_game/js/main.js
+++ Wk 2/goal4_assign_guessing_game/js/main.js
@@ -13,7 +13,7 @@ var input = document.getElementById("input");	// Gets input text field element
 var output = document.getElementById("output");	//	Gets ouput text element
 var button = document.getElementById("button");	// Gets the button element
 var userGuess = 0;	// Stores the user's guess.
-var numGuess = 3;	// The number of guesses the user may have
+var numGuess = 0;	// The number of guesses used
 
 
 
@@ -22,10 +22,12 @@ var numGuess = 3;	// The number of guesses the user may have
 
 	// Generates random integer 1-10
 	pcNum = Math.floor(Math.random() * 10 + 1);
+	//	console.log("The target number is: " + pcNum);	// Can be used to see the target number (cheats)
 	
 	// Adds the event listener to the button
 	button.addEventListener("click", checkGuess, false);
 	
+	// Function used for the onClick event handler
 	function checkGuess(){
 		
 		// Stores the user input
@@ -33,14 +35,24 @@ var numGuess = 3;	// The number of guesses the user may have
 		
 		// Checks the input to be a number 1-10
 		if(isNumber(userGuess) && userGuess <=10 && userGuess >=1){
+			// Adds 1 to the guess counter
+			numGuess++;
 			
+			// Checks for a winner, or tells the user higher or lower
 			if(pcNum == userGuess){
 				output.innerHTML = "YOU WIN!! The number was: " + pcNum;
+				button.removeEventListener("click", guessCheck);	// Disables the click event for the button
 			}else if(userGuess > pcNum){
-				output.innerHTML = "TOO HIGH! Try again!";
+				output.innerHTML = "That's too high. Guess: " + numGuess + ", Remaining: " + (3-numGuess);
 			}else if(userGuess < pcNum){
-				output.innerHTML = "TOO LOW! Try again!";
+				output.innerHTML = "That's too low. Guess: " + numGuess + ", Remaining: " + (3-numGuess);
 			}
+			
+			if(numGuess > 2){
+				output.innerHTML = "No more guesses left! The number was: " + pcNum;
+				button.removeEventListener("click", checkGuess);	// Disables the click event for the button
+			}
+		// Catch statement prompting the user to try a different input	
 		}else{
 			output.innerHTML = "Please enter a number, 1-10";
 		}
