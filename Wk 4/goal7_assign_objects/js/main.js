8d5f7612af744da8505bef0627830d3001cc29c7
Jason <jason.kastrup@gmail.com>
2014-05-30 17:58:56 -0400

    wk 4 Person Object
    Person.js updated (possibly finished?)
    Starting min.’s

--- Wk 4/goal7_assign_objects/js/main.js
+++ Wk 4/goal7_assign_objects/js/main.js
@@ -8,7 +8,13 @@
 	
 	// Array of names to be randomly selected
 	var names = ["Jason", "John", "Lauren", "Sara", "Garek"];
-
+	// Stores the total number of people to be used
+	var numPeople = 3;
+	// An array that will be used to store the created Person objects
+	var allPeople = [];
+	// Will store the interval timer to update each person's action
+	var interval;
+	
 
 
 
--- Wk 4/goal7_assign_objects/js/person.js
+++ Wk 4/goal7_assign_objects/js/person.js
@@ -4,10 +4,13 @@
  * Goal: Goal7
  */
 
-
+// Self-executing function
 (function(){
 	
+	// Adds the Person object to the global window object
 	window.Person = Person;
+	
+	// Creates arrays to be used for random selection
 	Person.jobs = ["Mechanic", "Astronaut", "Field Biologist", "Google CEO"];
 	Person.actions = ["Sleep", "Work", "Potty Break", "Out on the town", "M.I.A."];
 
@@ -23,9 +26,22 @@
 		this.job = Person.jobs[Math.floor(Math.random() * Person.jobs.length)];
 		this.action = Person.actions[Math.floor(Math.random() * Person.actions.length)];
 		
-		// Displays the first action of the person in the proper row
+		// Displays the first action of the person in the proper row (Actions will be updated randomly)
 		var elementID = document.getElementById("R" + this.row + "c3");
 		elementID.innerHTML = this.action;
+	}
+	
+	Person.prototype.update = function(){
+		if(Math.floor(Math.random() < .01)){
+			
+			// Creates variable holders for an array index and element ID 
+			var count = Math.floor(Math.random() * Person.actions.length);
+			var elementID = document.getElementById("r" + this.row + "c3");
+			
+			// Uses the array index and element ID above to update the action and the form
+			this.action = Person.actions[count];
+			elementID.innerHTML = this.action;
+		}
 	}
 
 	
